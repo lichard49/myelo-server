b4c2c2f593403984cebe7ac516ca544eed0ce0cd
Richard Li <lichard49@gmail.com>
2022-01-16 00:02:14 -0800

    Create WebSocket connection between server and visualization client

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi import WebSocket
+from fastapi import WebSocketDisconnect
 from fastapi import Request
 from fastapi.templating import Jinja2Templates
 
@@ -7,10 +8,23 @@ from fastapi.templating import Jinja2Templates
 templates = Jinja2Templates(directory='templates')
 app = FastAPI()
 
+connected_visualization_sockets = []
 
 @app.get('/')
 async def home(request: Request):
 	return templates.TemplateResponse('index.html', {'request': request})
+
+
+@app.websocket('/visualization_socket')
+async def visualization_socket(websocket: WebSocket):
+  await websocket.accept()
+  connected_visualization_sockets.append(websocket)
+
+  try:
+    while True:
+      await websocket.receive_text()
+  except WebSocketDisconnect:
+    connected_visualization_sockets.remove(websocket)
 
 
 @app.websocket('/websocket_endpoint')
