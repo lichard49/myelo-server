f1e27f75dee798ad3f9546d296d1b6648c6859c0
Richard Li <lichard49@gmail.com>
2022-01-16 01:44:09 -0800

    Import Chart.js and plot data received from WebSocket

--- client_test.py
+++ client_test.py
@@ -4,11 +4,13 @@ from websockets import connect
 import random
 
 
-async def sendRandomNumbers(uri, n=1000):
+async def sendRandomNumbers(uri, n=1000, i=3, fs=10):
+  period = 1/fs
   async with connect(uri) as websocket:
     for _ in range(n):
-      await websocket.send(str(random.randint(0, 1024)))
-      await asyncio.sleep(0.1)
+      data = ','.join([str(random.randint(0, 1024)) for _ in range(i)])
+      await websocket.send(data)
+      await asyncio.sleep(period)
 
 
 asyncio.run(sendRandomNumbers('ws://127.0.0.1:8000/sockets/input'))
