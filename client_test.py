17e4a872cec2170d92932ee8e70da56e3725e9ce
Richard Li <lichard49@gmail.com>
2022-01-31 01:42:48 -0800

    Started plotting on raw canvas; Chart.js wasn't fast enough to keep up with streaming data

--- client_test.py
+++ client_test.py
@@ -2,13 +2,23 @@ import asyncio
 from websockets import connect
 
 import random
+import math
+
+
+def getRandomNumbers(i):
+  return ','.join([str(random.randint(0, 1024)) for _ in range(i)])
+
+
+def getSineWave(i, j):
+  return ','.join([str(math.sin(j * 0.2) * (k + 1)) for k in range(i)])
 
 
 async def sendRandomNumbers(uri, n=1000, i=3, fs=10):
   period = 1/fs
   async with connect(uri) as websocket:
-    for _ in range(n):
-      data = ','.join([str(random.randint(0, 1024)) for _ in range(i)])
+    for j in range(n):
+      # data = getRandomNumbers(i)
+      data = getSineWave(i, j)
       await websocket.send(data)
       await asyncio.sleep(period)
 
