2e1d9770e2cb8d32cb33c95512d3de91a25a8967
Richard Li <lichard49@gmail.com>
2022-01-31 23:58:47 -0800

    Implemented saving data to disk on the server

--- server.py
+++ server.py
@@ -4,15 +4,43 @@ from fastapi import WebSocketDisconnect
 from fastapi import Request
 from fastapi.templating import Jinja2Templates
 
+import time
+import os
+
 
 templates = Jinja2Templates(directory='templates')
 app = FastAPI()
 
 connected_output_sockets = []
 
+recording = False
+record_file = None
+
+
 @app.get('/')
 async def home(request: Request):
 	return templates.TemplateResponse('index.html', {'request': request})
+
+
+@app.get('/record')
+async def record(request: Request):
+  global recording, record_file
+
+  recording_request = request.query_params['recording']
+
+  if recording_request == 'true':
+    record_file_name = time.strftime('%Y_%m_%d-%H_%M_%S',
+      time.localtime(time.time()))
+    record_file_path = os.path.join('data', record_file_name + '.csv')
+    record_file = open(record_file_path, 'w')
+    recording = True
+    return 1
+  elif recording_request == 'false':
+    recording = False
+    record_file.close()
+    return 0
+
+  return -1
 
 
 @app.websocket('/sockets/output')
@@ -32,5 +60,9 @@ async def input_socket(websocket: WebSocket):
   await websocket.accept()
   while True:
     data = await websocket.receive_text()
+
+    if recording:
+      record_file.write(data + '\n')
+
     for output_socket in connected_output_sockets:
       await output_socket.send_text(data)
